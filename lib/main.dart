6fbea4055e4edb825b9798a6d155dd3b38af89f8
dean <dean@baejin-us-MacBook-Pro.local>
2024-04-15 09:19:44 +0900

    BuildContext에 대한 이해

--- lib/main.dart
+++ lib/main.dart
@@ -198,12 +198,20 @@ class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: const Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              const MyLargeTitle(),
               const Text(
                 "Click Count",
                 style: TextStyle(
@@ -225,6 +233,23 @@ class _MyWidgetState extends State<MyWidget> {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Larg Text',
+      style: TextStyle(
+        fontSize: 50,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
