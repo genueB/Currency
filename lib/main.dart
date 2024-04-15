6aa29320452b6a2fd8f676e1320de715fe311c24
dean <dean@baejin-us-MacBook-Pro.local>
2024-04-15 09:08:41 +0900

    StatefulWidget 기본 사용법

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,7 @@ import 'package:toonflix/widgets/button.dart';
 import 'package:toonflix/widgets/currency_card.dart';
 
 void main() {
-  // runApp(const MyApp());
-
-  var nico = Player("nico");
-  runApp(const MyApp());
+  runApp(const MyWidget());
 }
 
 // root Widget
@@ -187,10 +184,48 @@ class MyWidget extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyWidget> {
-  int counter = 0;
+  List<int> numbers = [];
+  // int counter = 0;
+
+  void onClicked() {
+    setState(() {
+      // counter += 1;
+      numbers.add(numbers.length);
+      print(numbers);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Click Count",
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              for (var n in numbers)
+                Text(
+                  '$n',
+                  style: const TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+              IconButton(
+                onPressed: onClicked,
+                icon: const Icon(Icons.add_box_rounded),
+                iconSize: 40,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
